bb22a143dc1b8d3cbe2cb4d592e9f7597452e443
懒散兔 <zerberrabbit@gmail.com>
2022-03-01 01:32:23 +0800

    feat(twitter):complete the keyOffset function

--- indexer/pkg/api/twitter/twitter.go
+++ indexer/pkg/api/twitter/twitter.go
@@ -8,12 +8,35 @@ import (
 
 const endpoint = "https://api.twitter.com/1.1"
 
-func UserShow(name string) {
-	url := fmt.Sprintf("%s/users/show.json?screen_name=%s", endpoint, name)
+func GetFields() {
 
-	response, _ := util.Get(url, headers)
 }
 
-func UserTimeline(name string, count int, useCache bool) {
+func GetTimeline() {
 
+}
+
+func userShow(name string) (string, error) {
+	key := util.GotKey("round-robin", "Twitter")
+	authorization := fmt.Sprintf("Bearer %s", key)
+
+	var headers = map[string]string{
+		"accept":        "application/json",
+		"Authorization": authorization,
+	}
+	url := fmt.Sprintf("%s/users/show.json?screen_name=%s", endpoint, name)
+
+	response, err := util.Get(url, headers)
+	if err != nil {
+		return nil, err
+	}
+
+}
+
+func userTimeline(name string, count int, useCache bool) {
+	url := fmt.Sprintf("%s/statuses/user_timeline.json?screen_name=%s&count=%d&exclude_replies=true", endpoint, name, count)
+}
+
+func formatTweetText(tweet *Tweet) string {
+	return fmt.Sprintf("%s: %s", tweet.User.Name, tweet.Text)
 }
--- indexer/pkg/util/util.go
+++ indexer/pkg/util/util.go
@@ -23,3 +23,35 @@ func Post(url string, headers map[string]string, data string) ([]byte, error) {
 
 	return resp.Body(), err
 }
+
+var keyOffset map[string]int
+
+func GotKey(strategy string, indexer_id string, keys []string) string {
+	if len(strategy) == 0 {
+		strategy = "round-robin"
+	}
+	if len(indexer_id) == 0 {
+		indexer_id = "."
+	}
+
+	var offset int
+	var key string
+
+	if strategy == "first-always" {
+		key = "Bearer " + indexer_id
+	} else {
+
+		count, ok := keyOffset[indexer_id]
+
+		if !ok {
+			keyOffset[indexer_id] = 0
+		}
+
+		offset = count % len(keys)
+		keyOffset[indexer_id] = count + 1
+		key = keys[offset]
+	}
+
+	return key
+
+}
