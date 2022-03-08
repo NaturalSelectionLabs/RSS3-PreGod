84568cb272f888d0bcc90b101a5a3688e88762e9
懒散兔 <zerberrabbit@gmail.com>
2022-03-08 14:37:52 +0800

    feat:temporarily saved the new type of twitter

--- indexer/pkg/api/twitter/twitter.go
+++ indexer/pkg/api/twitter/twitter.go
@@ -5,30 +5,44 @@ import (
 
 	"github.com/NaturalSelectionLabs/RSS3-PreGod/indexer/pkg/util"
 	"github.com/NaturalSelectionLabs/RSS3-PreGod/shared/pkg/config"
+	"github.com/NaturalSelectionLabs/RSS3-PreGod/shared/pkg/logger"
+	"github.com/valyala/fastjson"
 )
 
 const endpoint = "https://api.twitter.com/1.1"
+
+var parser fastjson.Parser
 
 func GetTimeline() {
 
 }
 
-func GetFields(name string) (string, error) {
+func GetUsersShow(name string) (string, error) {
 	key := util.GotKey("round-robin", "Twitter", config.Config.Indexer.Twitter.Tokens)
 	authorization := fmt.Sprintf("Bearer %s", key)
+	logger.Infof("authorization: %s", authorization)
 	var result string
 
 	var headers = map[string]string{
-		"accept":        "application/json",
 		"Authorization": authorization,
 	}
 
 	url := fmt.Sprintf("%s/users/show.json?screen_name=%s", endpoint, name)
 
 	response, err := util.Get(url, headers)
+	if err != nil {
 		return result, err
-		return nil, err
 	}
+
+	userShow = new(UserShow)
+
+	parsedJson, err := parser.Parse(string(response))
+
+	if err != nil {
+		return result, err
+	}
+
+	userShow.Name = parsedJson.GetObject("name").GetString("name")
 
 	// 这里缺一个转化函数，response转化为struct结构体
 
--- indexer/pkg/api/twitter/type.go
+++ indexer/pkg/api/twitter/type.go
@@ -1,1 +1,15 @@
 package twitter
+
+type UserShow struct {
+	Name        string
+	Description string
+	ScreenName  string
+}
+
+type ContentInfo struct {
+	Title      string
+	PreContent string
+	Timestamp  string
+	Hash       string
+	Link       string
+}
