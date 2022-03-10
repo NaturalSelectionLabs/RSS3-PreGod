efb984ead952d8fde0c11e864a2866318e1418f6
Nya Candy <dev@candinya.com>
2022-03-10 15:23:46 +0800

    feat: update sign verify demo

--- hub/pkg/util/nacl/verify_test.go
+++ hub/pkg/util/nacl/verify_test.go
@@ -24,10 +24,10 @@ func TestVerify(t *testing.T) {
 	}{
 		{
 			name: "valid",
-			args: args{
-				msg:    `'[["_id",[["$oid","61c54ec1a3bae407078a1a68"]]],["content",[["_pass",[["assets",[["0",[["id","EVM+-0x000000000A38444e0a6E37d3b630d7e855a7cb13-Polygon.NFT-0xb4d21caf1cc3dadec5eecf753f5fc23094ddfb65.736"]]],["1",[["id","EVM+-0x000000000A38444e0a6E37d3b630d7e855a7cb13-Polygon.NFT-0xb766abf815d9e60a79ed96eac1093fbe46f31655.95314"],["order",1]]],["2",[["hide",true],["id","EVM+-0x000000000A38444e0a6E37d3b630d7e855a7cb13-Polygon.NFT-0xdf9293c820d5ac290b97fa62b449a9c790014296.1473"]]],["3",[["hide",true],["id","EVM+-0x000000000A38444e0a6E37d3b630d7e855a7cb13-Polygon.NFT-0x3c7174c46f6aeca7f485665a8ccfa29047f880e8.29"]]],["4",[["hide",true],["id","EVM+-0x000000000A38444e0a6E37d3b630d7e855a7cb13-Polygon.NFT-0x1fdf97e5bee48893eef28116973ca81166e4ec02.884"]]]]]]],["agent_id","NItLuV6qn0hybnExRU4Q42/ISGqYMpI2VHmbRcPdka0="],["assets",[["list_auto","0x000000000A38444e0a6E37d3b630d7e855a7cb13-list-assets.auto-0"]]],["backlinks",[]],["date_created","2021-12-20T15:56:03.737Z"],["date_updated","2021-12-25T07:57:34.114Z"],["id","0x000000000A38444e0a6E37d3b630d7e855a7cb13"],["items",[["list_auto","0x000000000A38444e0a6E37d3b630d7e855a7cb13-list-items.auto-0"]]],["profile",[["avatar",[["0","https://infura-ipfs.io/ipfs/QmcK8FSTtLQVydLEDKLv1hEacLxZgi7j2i4mkQQMyKxv6k"]]],["bio","Just a lazy man =U=<SITE#watertim.dev/>"],["name","WaterTim"]]],["version","rss3.io/version/v0.3.1"]]],["path","0x000000000A38444e0a6E37d3b630d7e855a7cb13"]]'`, //nolint:lll // long str needed
-				sig:    "Kz47gL88TIJPnHcNBld5dLe7P005AFPnN/haqSPPyufb6/ZXdiQNvZVHI0ImpWUhhMPRnh6tGIZnpX45wcMCBQ==",
-				pubkey: "MQ4rRFwbMa8BNsf8r+r/mCLT1qiZotWaayq//iMdbDc=",
+			args: args{ // Demo data from RSS3 v0.3.1 [0x1469Bd38ef9cF15917d052D6B10267ea041Ba091](https://prenode.rss3.dev/0x1469Bd38ef9cF15917d052D6B10267ea041Ba091)
+				msg:    `[["agent_id","AEop+wb5O1eTBe5Q2DQ4ZAD0McOgggeWm6P4ZmNovCg="],["assets",[["list_auto","0x1469Bd38ef9cF15917d052D6B10267ea041Ba091-list-assets.auto-0"]]],["backlinks",[]],["date_created","2021-09-03T13:58:39.005Z"],["date_updated","2022-03-10T07:20:47.485Z"],["id","0x1469Bd38ef9cF15917d052D6B10267ea041Ba091"],["items",[["list_auto","0x1469Bd38ef9cF15917d052D6B10267ea041Ba091-list-items.auto-0"]]],["links",[["0",[["id","following"],["list","0x1469Bd38ef9cF15917d052D6B10267ea041Ba091-list-links.following-0"]]]]],["profile",[["accounts",[["0",[["id","Twitter-lc825"],["tags",[["0","pass:order:0"]]]]]]],["avatar",[["0","https://rss3.mypinata.cloud/ipfs/Qmakn9hSmDqqB7o5eN2j2GP6UrPUhFk6SzpVD9vP2EttiG"]]],["bio","Lemon!!!!"],["name","Lemon"]]],["version","rss3.io/version/v0.3.1"]]`, //nolint:lll // long str needed
+				sig:    "VNrLH+SpbkjO1stOuH9u6mpk5Z57T42LZJWu+NXo4D++0SsdYdfdMT3XwMQQBq+9zUyYnaWta82lnUTXshQbCg==",
+				pubkey: "AEop+wb5O1eTBe5Q2DQ4ZAD0McOgggeWm6P4ZmNovCg=",
 			},
 			want: true,
 		},
