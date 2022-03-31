c8a9c3a25c6aab2351a11ddbf88ae33b6081494e
Songkeys <songv587@gmail.com>
2022-03-31 15:05:09 +0800

    merge fix_indexer_param into v0.4.0-rc.2 (#84)
    Co-authored-by: 懒散兔 <zerberrabbit@gmail.com>

--- indexer/pkg/router/api/item.go
+++ indexer/pkg/router/api/item.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/NaturalSelectionLabs/RSS3-PreGod/indexer/pkg/autoupdater"
@@ -19,8 +20,8 @@ import (
 type GetItemRequest struct {
 	Identity   string               `form:"proof" binding:"required"`
 	PlatformID constants.PlatformID `form:"platform_id" binding:"required"`
-	NetworkID  constants.NetworkID  `form:"network_id" binding:"required"`
-	ItemType   constants.ItemType   `form:"item_type" binding:"required"`
+	NetworkID  constants.NetworkID  `form:"network_id"`
+	ItemType   constants.ItemType   `form:"item_type"`
 }
 
 type itemsResult struct {
@@ -48,15 +49,28 @@ func GetItemHandlerFunc(c *gin.Context) {
 
 	if len(request.Identity) <= 0 ||
 		!constants.IsValidPlatformSymbol(string(request.PlatformID.Symbol())) ||
-		!constants.IsValidNetworkName(string(request.NetworkID.Symbol())) ||
-		request.ItemType == constants.ItemTypeUnknown {
+		!constants.IsValidNetworkName(string(request.NetworkID.Symbol())) {
 		logger.Errorf("parameter error")
 
 		response.ErrorBase = util.GetErrorBase(util.ErrorCodeParameterError)
 		c.JSON(http.StatusOK, response)
+
+		return
 	}
 
-	dbResult, err := getItemsFromDB(c, request)
+	if len(request.ItemType) > 0 &&
+		request.ItemType != constants.ItemTypeAsset &&
+		request.ItemType != constants.ItemTypeNote {
+		logger.Errorf("parameter error")
+
+		response.ErrorBase = util.GetErrorBase(util.ErrorCodeParameterError)
+		c.JSON(http.StatusOK, response)
+
+		return
+	}
+
+
+	dbResult, err := getItemsFromDB(c.Request.Context(), request)
 	if err != nil {
 		logger.Errorf("get items from db error: %s", err.Error())
 	}
@@ -69,20 +83,13 @@ func GetItemHandlerFunc(c *gin.Context) {
 		return
 	}
 
-	getItemHandler := crawler_handler.NewGetItemsHandler(crawler.WorkParam{
-		Identity:   request.Identity,
-		PlatformID: request.PlatformID,
-		NetworkID:  platform2Network[request.PlatformID],
-	})
-	handlerResult := getItemHandler.Excute()
+	logger.Infof("GetItemHandlerFunc3")
 
-	if handlerResult == nil || handlerResult.Result == nil {
-		logger.Errorf("[%s] get item error", request.Identity)
+	result, errorBase := getItemsResult(request)
+	response.ErrorBase = errorBase
 
-		response.ErrorBase = util.GetErrorBase(util.ErrorCodeNotFoundData)
-		c.JSON(http.StatusOK, response)
-
-		return
+	if response.ErrorBase.ErrorCode == 0 {
+		response.ItemsResult = *result
 	}
 
 	if handlerResult.Error.ErrorCode != util.ErrorCodeSuccess {
@@ -90,16 +97,10 @@ func GetItemHandlerFunc(c *gin.Context) {
 
 		response.ErrorBase = util.GetErrorBase(handlerResult.Error.ErrorCode)
 		c.JSON(http.StatusOK, response)
-
-		return
 	}
-
-	response.ItemsResult = *getItemsFromCrawlerHandler(handlerResult.Result.Items, request.ItemType)
-
-	c.JSON(http.StatusOK, response)
 }
 
-func getItemsFromDB(c *gin.Context, request GetItemRequest) (*itemsResult, error) {
+func getItemsFromDB(context context.Context, request GetItemRequest) (*itemsResult, error) {
 	ai := rss3uri.NewAccountInstance(request.Identity, request.PlatformID.Symbol())
 
 	var err error
@@ -108,10 +109,10 @@ func getItemsFromDB(c *gin.Context, request GetItemRequest) (*itemsResult, error
 
 	isOld, err := db.Exists(ai)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("find db exists false:%s", err)
 	}
 
-	addToRecentVisit(c.Request.Context(), &request)
+	addToRecentVisit(context, &request)
 
 	if !isOld {
 		return nil, nil
@@ -121,22 +122,22 @@ func getItemsFromDB(c *gin.Context, request GetItemRequest) (*itemsResult, error
 	case constants.ItemTypeNote:
 		result.NoteItems, err = db.GetAccountItems(ai, constants.ItemTypeNote)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("get notes items error:%s", err)
 		}
 	case constants.ItemTypeAsset:
 		result.AssetItems, err = db.GetAccountItems(ai, constants.ItemTypeAsset)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("get asset items error:%s", err)
 		}
 	default:
 		result.NoteItems, err = db.GetAccountItems(ai, constants.ItemTypeNote)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("get notes items error:%s", err)
 		}
 
 		result.AssetItems, err = db.GetAccountItems(ai, constants.ItemTypeAsset)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("get asset items error:%s", err)
 		}
 	}
 
@@ -179,4 +180,66 @@ func addToRecentVisit(ctx context.Context, req *GetItemRequest) error {
 	}
 
 	return autoupdater.AddToRecentVisitQueue(ctx, param)
+}
+
+func getItemsResultFromOneNetwork(identity string,
+	platformID constants.PlatformID,
+	networkID constants.NetworkID,
+	itemType constants.ItemType) (*itemsResult, util.ErrorBase) {
+	getItemHandler := crawler_handler.NewGetItemsHandler(crawler.WorkParam{
+		Identity:   identity,
+		PlatformID: platformID,
+		NetworkID:  platform2Network[platformID],
+	})
+
+	handlerResult := getItemHandler.Excute()
+
+	if handlerResult == nil || handlerResult.Result == nil {
+		return nil, util.GetErrorBase(util.ErrorCodeNotFoundData)
+	}
+
+	if handlerResult.Error.ErrorCode != util.ErrorCodeSuccess {
+		logger.Errorf("[%s] get item error", identity)
+
+		return nil, util.GetErrorBase(handlerResult.Error.ErrorCode)
+	}
+
+	return getItemsFromCrawlerHandler(handlerResult.Result.Items, itemType), util.GetErrorBase(util.ErrorCodeSuccess)
+}
+
+func setItemsResult(originalItems *[]model.Item, addedItems *[]model.Item) {
+	if originalItems == nil {
+		originalItems = new([]model.Item)
+	}
+
+	if addedItems != nil {
+		curraddedItems := *addedItems
+		*originalItems = append(*originalItems, curraddedItems...)
+	}
+}
+
+func getItemsResult(request GetItemRequest) (*itemsResult, util.ErrorBase) {
+	result := new(itemsResult)
+	errorBase := util.GetErrorBase(util.ErrorCodeSuccess)
+
+	if request.NetworkID == constants.NetworkIDUnknown {
+		networkIDs := constants.GetEthereumPlatformNetworks()
+		for _, networkID := range networkIDs {
+			currResult, currErrorBase := getItemsResultFromOneNetwork(
+				request.Identity, request.PlatformID, networkID, request.ItemType)
+
+			if currErrorBase.ErrorCode != util.ErrorCodeSuccess {
+				logger.Errorf("[%s] get item error, network[%s],error reason:%s",
+					request.Identity, networkID.Symbol(), currErrorBase.ErrorMsg)
+			}
+
+			setItemsResult(result.AssetItems, currResult.AssetItems)
+			setItemsResult(result.NoteItems, currResult.NoteItems)
+		}
+	} else {
+		result, errorBase = getItemsResultFromOneNetwork(
+			request.Identity, request.PlatformID, request.NetworkID, request.ItemType)
+	}
+
+	return result, errorBase
 }
