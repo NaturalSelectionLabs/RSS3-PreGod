bcb2b993ae8ad508e39c5aa97f8a2434845ff105
KallyDev <kallydev@gmail.com>
2022-03-31 15:33:26 +0800

    fix: wrong database primary key

--- hub/database/model/profile.go
+++ hub/database/model/profile.go
@@ -12,9 +12,9 @@ import (
 var _ schema.Tabler = &Profile{}
 
 type Profile struct {
-	ID          string               `gorm:"column:id;primaryKey"`
-	Platform    int                  `gorm:"column:platform"`
-	Source      int                  `gorm:"column:source"`
+	ID          string               `gorm:"column:id;index:index_profile_id"`
+	Platform    int                  `gorm:"column:platform;index_profile_platform"`
+	Source      int                  `gorm:"column:source;index_profile_source"`
 	Name        sql.NullString       `gorm:"column:name"`
 	Bio         sql.NullString       `gorm:"column:bio"`
 	Avatars     pq.StringArray       `gorm:"column:avatars;type:text[]"`
