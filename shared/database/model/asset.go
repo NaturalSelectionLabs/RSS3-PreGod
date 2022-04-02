b50c001c394af43cdb9518323f531dc14f290e8e
KallyDev <kallydev@gmail.com>
2022-04-02 12:39:55 +0800

    feat(shared): add metadata and owner field

--- shared/database/model/asset.go
+++ shared/database/model/asset.go
@@ -10,17 +10,20 @@ import (
 var _ schema.Tabler = &Asset{}
 
 type Asset struct {
-	Identifier  string         `gorm:"column:identifier"`
-	RelatedURLs pq.StringArray `gorm:"column:related_urls;type:text[]"`
-	Links       string         `gorm:"column:links"`
-	BackLinks   string         `gorm:"column:backlinks"`
-	Tags        pq.StringArray `gorm:"column:tags;type:text[]"`
-	Authors     pq.StringArray `gorm:"column:authors;type:text[]"`
-	Title       string         `gorm:"column:title"`
-	Summary     string         `gorm:"column:summary"`
-	Attachments datatypes.JSON `gorm:"column:attachments"`
-	Source      string         `gorm:"column:source"`
-	Metadata    datatypes.JSON `gorm:"column:metadata"`
+	Identifier      string         `gorm:"column:identifier"`
+	Owner           string         `gorm:"colum:owner"`
+	RelatedURLs     pq.StringArray `gorm:"column:related_urls;type:text[]"`
+	Links           string         `gorm:"column:links"`
+	BackLinks       string         `gorm:"column:backlinks"`
+	Tags            pq.StringArray `gorm:"column:tags;type:text[]"`
+	Authors         pq.StringArray `gorm:"column:authors;type:text[]"`
+	Title           string         `gorm:"column:title"`
+	Summary         string         `gorm:"column:summary"`
+	Attachments     datatypes.JSON `gorm:"column:attachments"`
+	Source          string         `gorm:"column:source"`
+	MetadataNetwork string         `gorm:"column:metadata_network"`
+	MetadataProof   string         `gorm:"column:metadata_proof"`
+	Metadata        datatypes.JSON `gorm:"column:metadata"`
 
 	common.Table
 }
