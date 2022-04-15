e821fb5523c667859bc4b9d1fa2ee97d8ba474c7
KallyDev <kallydev@gmail.com>
2022-04-15 16:47:01 +0800

    feat(shared): metadata network and proof for profile

--- shared/database/model/profile.go
+++ shared/database/model/profile.go
@@ -12,14 +12,16 @@ import (
 var _ schema.Tabler = &Profile{}
 
 type Profile struct {
-	ID          string               `gorm:"column:id;index:index_profile_id"`
-	Platform    int                  `gorm:"column:platform;index_profile_platform"`
-	Source      int                  `gorm:"column:source;index_profile_source"`
-	Name        sql.NullString       `gorm:"column:name"`
-	Bio         sql.NullString       `gorm:"column:bio"`
-	Avatars     pq.StringArray       `gorm:"column:avatars;type:text[]"`
-	Attachments datatype.Attachments `gorm:"column:attachments;type:jsonb"`
-	Metadata    string               `gorm:"metadata"`
+	ID              string               `gorm:"column:id;index:index_profile_id"`
+	Platform        int                  `gorm:"column:platform;index_profile_platform"`
+	Source          int                  `gorm:"column:source;index_profile_source"`
+	Name            sql.NullString       `gorm:"column:name"`
+	Bio             sql.NullString       `gorm:"column:bio"`
+	Avatars         pq.StringArray       `gorm:"column:avatars;type:text[]"`
+	Attachments     datatype.Attachments `gorm:"column:attachments;type:jsonb"`
+	MetadataNetwork string               `gorm:"column:metadata_network"`
+	MetadataProof   string               `gorm:"column:metadata_proof"`
+	Metadata        string               `gorm:"metadata"`
 
 	common.Table
 }
