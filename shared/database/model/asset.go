8b62040c9271257bd70520b883f0dabeb3aea957
KallyDev <kallydev@gmail.com>
2022-04-19 16:51:25 +0800

    feat(shared): create table index

--- shared/database/model/asset.go
+++ shared/database/model/asset.go
@@ -13,20 +13,20 @@ var _ schema.Tabler = &Asset{}
 
 type Asset struct {
 	Identifier      string         `gorm:"column:identifier;primaryKey"`
-	Owner           string         `gorm:"colum:owner"`
-	ProfileSourceID int            `gorm:"profile_source_id"`
+	Owner           string         `gorm:"colum:owner;index:index_asset_owner"`
+	ProfileSourceID int            `gorm:"profile_source_id;index:index_asset_profile_source_id"`
 	RelatedURLs     pq.StringArray `gorm:"column:related_urls;type:text[]"`
 	Tags            pq.StringArray `gorm:"column:tags;type:text[]"`
 	Authors         pq.StringArray `gorm:"column:authors;type:text[]"`
 	Title           string         `gorm:"column:title"`
 	Summary         string         `gorm:"column:summary"`
 	Attachments     datatypes.JSON `gorm:"column:attachments;default:'{}'"`
-	Source          string         `gorm:"column:source"`
+	Source          string         `gorm:"column:source;index:index_asset_source"`
 	MetadataNetwork string         `gorm:"column:metadata_network"`
 	MetadataProof   string         `gorm:"column:metadata_proof"`
 	Metadata        datatypes.JSON `gorm:"column:metadata;default:'{}'"`
-	DateCreated     time.Time      `gorm:"column:date_created"`
-	DateUpdated     time.Time      `gorm:"column:date_updated"`
+	DateCreated     time.Time      `gorm:"column:date_created;index:index_asset_date_created"`
+	DateUpdated     time.Time      `gorm:"column:date_updated;index:index_asset_date_updated"`
 
 	common.Table
 }
