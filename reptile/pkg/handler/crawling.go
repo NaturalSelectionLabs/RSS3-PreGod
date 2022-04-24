584f2e1d25a08ff2b13a66d11a9463885faeca54
Zerber <zerberrabbit@gmail.com>
2022-04-24 18:02:56 +0800

    feat:int to int64 unit

---
+++ reptile/pkg/handler/crawling.go
@@ -0,0 +1,60 @@
+package handler
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/NaturalSelectionLabs/RSS3-PreGod/indexer/pkg/api/gitcoin"
+	"github.com/NaturalSelectionLabs/RSS3-PreGod/shared/pkg/httpx"
+	"github.com/NaturalSelectionLabs/RSS3-PreGod/shared/pkg/logger"
+	"github.com/valyala/fastjson"
+)
+
+const gitcoinPrefix = "https://gitcoin.co/grants/v1/api/grant/"
+
+func GetResult(pos int64) (*gitcoin.ProjectInfo, error) {
+	info := new(gitcoin.ProjectInfo)
+	url := gitcoinPrefix + strconv.FormatInt(pos, 10)
+	// logger.Infof("url: %s", url)
+	result, err := httpx.Get(url, nil)
+
+	if err != nil {
+		return nil, fmt.Errorf("get result false:%s", err)
+	}
+
+	if result == nil {
+		return nil, nil
+	}
+
+	var parser fastjson.Parser
+	parsedJson, parseErr := parser.Parse(string(result))
+
+	if parseErr != nil {
+		return nil, fmt.Errorf("get result false:%s", parseErr)
+	}
+
+	if parsedJson.GetInt("status") != 200 {
+		logger.Warnf("get result [%d] not find: %s", pos,
+			string(parsedJson.GetStringBytes("message")))
+
+		return nil, nil
+	}
+
+	grantsObjectJson := parsedJson.Get("grants")
+
+	info.Active = grantsObjectJson.GetBool("active")
+	info.Id = grantsObjectJson.GetInt("id")
+	info.Title = string(grantsObjectJson.GetStringBytes("title"))
+	info.Slug = string(grantsObjectJson.GetStringBytes("slug"))
+	info.Description = string(grantsObjectJson.GetStringBytes("description"))
+	info.ReferUrl = string(grantsObjectJson.GetStringBytes("reference_url"))
+	info.Logo = string(grantsObjectJson.GetStringBytes("logo_url"))
+	info.AdminAddress = string(grantsObjectJson.GetStringBytes("admin_address"))
+	info.TokenAddress = string(grantsObjectJson.GetStringBytes("token_address"))
+	info.TokenSymbol = string(grantsObjectJson.GetStringBytes("token_symbol"))
+	info.ContractAddress = string(grantsObjectJson.GetStringBytes("contract_address"))
+
+	// logger.Infof("%v", info)
+
+	return info, nil
+}
---
+++ reptile/pkg/handler/db.go
@@ -0,0 +1,102 @@
+package handler
+
+import (
+	"fmt"
+
+	"github.com/NaturalSelectionLabs/RSS3-PreGod/indexer/pkg/api/gitcoin"
+	"github.com/NaturalSelectionLabs/RSS3-PreGod/shared/database"
+	"github.com/NaturalSelectionLabs/RSS3-PreGod/shared/database/model"
+	"github.com/NaturalSelectionLabs/RSS3-PreGod/shared/pkg/constants"
+	"github.com/NaturalSelectionLabs/RSS3-PreGod/shared/pkg/logger"
+)
+
+// var reptileDB *gorm.DB
+
+const GitcoinProjectIdentity = "gitcoin-project"
+const GitcoinProjectPlatformID = 1000
+const GitcoinProjectIdentity2 = "gitcoin-project2"
+const GitcoinProjectPlatformID2 = 1001
+
+func SetLastPostion(pos int64, identity string, platformID constants.PlatformID) error {
+	if _, err := database.CreateCrawlerMetadata(database.DB, &model.CrawlerMetadata{
+		AccountInstance: identity,
+		PlatformID:      platformID,
+		LastBlock:       pos,
+	}, true); err != nil {
+		return fmt.Errorf("set last position error: %s", err)
+	}
+
+	return nil
+}
+
+func GetLastPostion(identity string, platformID constants.PlatformID) int64 {
+	metadata, dbQcmErr := database.QueryCrawlerMetadata(database.DB, identity, platformID)
+	if dbQcmErr != nil {
+		logger.Errorf("query crawler metadata error: %s", dbQcmErr)
+
+		return 0
+	}
+
+	return metadata.LastBlock
+}
+
+func SetResultInDB(project *gitcoin.ProjectInfo) error {
+	if project == nil {
+		return fmt.Errorf("project is nil")
+	}
+
+	if err := database.DB.Clauses(database.NewCreateClauses(true)...).Create(project).Error; err != nil {
+		return fmt.Errorf("set result in db error: %s ", err)
+	}
+
+	return nil
+}
+
+func SetResultsInDB(projects []gitcoin.ProjectInfo) error {
+	if err := database.DB.Clauses(database.NewCreateClauses(true)...).Create(&projects).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func GetResultMax() int64 {
+	var max int64
+	max = 0
+
+	rows, err := database.DB.Table("reptile-gitcoin.data").Select("max(id)").Rows()
+	if err != nil {
+		logger.Errorf("get result max error: %s", err)
+	}
+
+	if rows.Next() {
+		err := rows.Scan(&max)
+		if err != nil {
+			logger.Errorf("get result max error: %s", err)
+		}
+	}
+
+	return max
+}
+
+func GetResultFromDB(pos int64, endpos int64) ([]gitcoin.ProjectInfo, error) {
+	var projects []gitcoin.ProjectInfo
+
+	internalDB := database.DB.
+		Where("id > ?", pos).
+		Where("id <= ?", endpos)
+
+	if err := internalDB.Find(&projects).Error; err != nil {
+		return nil, err
+	}
+
+	return projects, nil
+}
+
+func UpdateResultsInDB(project *gitcoin.ProjectInfo, adminAddress string) error {
+	if err := database.DB.Model(project).Where("id=?", project.Id).Update("admin_address", adminAddress).Error; err != nil {
+		logger.Errorf("update result in db error: %s", err)
+	}
+
+	return nil
+}
