63aa165a76e9f2650823ddd4714c293e282a22e0
Zerber <zerberrabbit@gmail.com>
2022-05-20 15:49:23 +0800

    feat: perfect db

--- clean_gitcoin/internal/db.go
+++ clean_gitcoin/internal/db.go
@@ -5,10 +5,17 @@ import (
 	"github.com/NaturalSelectionLabs/RSS3-PreGod/shared/database/model"
 )
 
-func GetDataFromDB() {
+func GetDataFromDB(limit int, offset int) ([]model.Note, int64, error) {
 	var notes []model.Note
 	internalDB := database.DB.
-		Where("attachments != '[]'and tags && '{\"Token\"}' order by date_created desc limit ? offset ?")
+		Where("attachments != '[]'and tags && '{\"Token\"}' order by date_created desc limit ? offset ?", limit, offset)
+
+	var count int64
+	if err := internalDB.Model(&model.Note{}).Count(&count).Error; err != nil {
+		return nil, 0, err
+	}
+
+	return noteList, count, nil
 }
 
 func ClearGitCoinData() {
