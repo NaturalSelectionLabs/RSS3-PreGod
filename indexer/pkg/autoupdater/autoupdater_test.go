eef898f5bc10b208d9b83c44ac085941bddb5a28
polebug <polebugfly@gmail.com>
2022-05-27 13:06:25 +0800

    update(indexer): remove misskey test

--- indexer/pkg/autoupdater/autoupdater_test.go
+++ indexer/pkg/autoupdater/autoupdater_test.go
@@ -3,19 +3,11 @@ package autoupdater_test
 import (
 	"context"
 	"log"
-	"testing"
-	"time"
 
-	"github.com/NaturalSelectionLabs/RSS3-PreGod/indexer/pkg/autoupdater"
-	"github.com/NaturalSelectionLabs/RSS3-PreGod/indexer/pkg/crawler"
 	"github.com/NaturalSelectionLabs/RSS3-PreGod/shared/database"
 	"github.com/NaturalSelectionLabs/RSS3-PreGod/shared/pkg/cache"
 	_ "github.com/NaturalSelectionLabs/RSS3-PreGod/shared/pkg/config"
-	"github.com/NaturalSelectionLabs/RSS3-PreGod/shared/pkg/constants"
 	_ "github.com/NaturalSelectionLabs/RSS3-PreGod/shared/pkg/logger"
-	"github.com/go-redis/redis/v8"
-	"github.com/stretchr/testify/assert"
-	"github.com/vmihailenco/msgpack"
 )
 
 func init() {
@@ -30,84 +22,84 @@ func init() {
 	}
 }
 
-func Test_Run(t *testing.T) {
-	ctx := context.Background()
+// func Test_Run(t *testing.T) {
+// 	ctx := context.Background()
 
-	// Test Add
-	for i := 0; i < 50; i++ {
-		autoupdater.AddToRecentVisitQueue(ctx, &crawler.WorkParam{
-			Identity:   "henry@nya.one",
-			NetworkID:  constants.NetworkIDMisskey,
-			PlatformID: constants.PlatformIDMisskey,
-			Limit:      100,
-			Timestamp:  time.Now().Add(-time.Hour),
-		})
-	}
-	time.Sleep(time.Second)
-	ts := time.Now().Unix()
-	rdb := cache.GetRedisClient()
-	zitems, err := rdb.ZRangeWithScores(ctx, autoupdater.RecentVisitQueue.Key, 0, -1).Result()
-	assert.Nil(t, err)
+// 	// Test Add
+// 	for i := 0; i < 50; i++ {
+// 		autoupdater.AddToRecentVisitQueue(ctx, &crawler.WorkParam{
+// 			Identity:   "henry@nya.one",
+// 			NetworkID:  constants.NetworkIDMisskey,
+// 			PlatformID: constants.PlatformIDMisskey,
+// 			Limit:      100,
+// 			Timestamp:  time.Now().Add(-time.Hour),
+// 		})
+// 	}
+// 	time.Sleep(time.Second)
+// 	ts := time.Now().Unix()
+// 	rdb := cache.GetRedisClient()
+// 	zitems, err := rdb.ZRangeWithScores(ctx, autoupdater.RecentVisitQueue.Key, 0, -1).Result()
+// 	assert.Nil(t, err)
 
-	identity2ts := []int64{}
+// 	identity2ts := []int64{}
 
-	for _, zitem := range zitems {
-		param := crawler.WorkParam{}
-		assert.Nil(t, msgpack.Unmarshal([]byte(zitem.Member.(string)), &param)) // nolint: forcetypeassert // in testing
-		identity2ts = append(identity2ts, int64(zitem.Score))
-	}
+// 	for _, zitem := range zitems {
+// 		param := crawler.WorkParam{}
+// 		assert.Nil(t, msgpack.Unmarshal([]byte(zitem.Member.(string)), &param)) // nolint: forcetypeassert // in testing
+// 		identity2ts = append(identity2ts, int64(zitem.Score))
+// 	}
 
-	assert.EqualValues(t, 50, len(identity2ts))
+// 	assert.EqualValues(t, 50, len(identity2ts))
 
-	for _, valueTS := range identity2ts {
-		assert.True(t, valueTS < ts)
-	}
+// 	for _, valueTS := range identity2ts {
+// 		assert.True(t, valueTS < ts)
+// 	}
 
-	// clear old
-	items, _, err := rdb.ZScan(ctx, autoupdater.RecentVisitQueue.Key, 0, "*", 0).Result()
-	assert.Nil(t, err)
+// 	// clear old
+// 	items, _, err := rdb.ZScan(ctx, autoupdater.RecentVisitQueue.Key, 0, "*", 0).Result()
+// 	assert.Nil(t, err)
 
-	itemsGot := len(items) / 2
+// 	itemsGot := len(items) / 2
 
-	for index, item := range items {
-		if index%2 != 0 { // score
-			continue
-		}
+// 	for index, item := range items {
+// 		if index%2 != 0 { // score
+// 			continue
+// 		}
 
-		rdb.ZAdd(ctx, autoupdater.RecentVisitQueue.Key, []*redis.Z{{
-			Score:  10086,
-			Member: item,
-		}}...)
-	}
+// 		rdb.ZAdd(ctx, autoupdater.RecentVisitQueue.Key, []*redis.Z{{
+// 			Score:  10086,
+// 			Member: item,
+// 		}}...)
+// 	}
 
-	count, err := rdb.ZCard(ctx, autoupdater.RecentVisitQueue.Key).Result()
-	assert.Nil(t, err)
-	assert.EqualValues(t, 50, count)
+// 	count, err := rdb.ZCard(ctx, autoupdater.RecentVisitQueue.Key).Result()
+// 	assert.Nil(t, err)
+// 	assert.EqualValues(t, 50, count)
 
-	autoupdater.RecentVisitQueue.ClearOld(ctx)
+// 	autoupdater.RecentVisitQueue.ClearOld(ctx)
 
-	count, err = rdb.ZCard(ctx, autoupdater.RecentVisitQueue.Key).Result()
-	assert.Nil(t, err)
-	assert.EqualValues(t, 50-itemsGot, count)
+// 	count, err = rdb.ZCard(ctx, autoupdater.RecentVisitQueue.Key).Result()
+// 	assert.Nil(t, err)
+// 	assert.EqualValues(t, 50-itemsGot, count)
 
-	// Iter and Run
-	// Remove 35, in most cases there will be 5 left.
-	// If left too many, crawl may be too slow
-	items, _, _ = rdb.ZScan(ctx, autoupdater.RecentVisitQueue.Key, 0, "*", 35).Result()
-	for index, item := range items { // expire 10
-		if index%2 != 0 { // score
-			continue
-		}
+// 	// Iter and Run
+// 	// Remove 35, in most cases there will be 5 left.
+// 	// If left too many, crawl may be too slow
+// 	items, _, _ = rdb.ZScan(ctx, autoupdater.RecentVisitQueue.Key, 0, "*", 35).Result()
+// 	for index, item := range items { // expire 10
+// 		if index%2 != 0 { // score
+// 			continue
+// 		}
 
-		rdb.ZAdd(ctx, autoupdater.RecentVisitQueue.Key, []*redis.Z{{
-			Score:  10086,
-			Member: item,
-		}}...)
-	}
+// 		rdb.ZAdd(ctx, autoupdater.RecentVisitQueue.Key, []*redis.Z{{
+// 			Score:  10086,
+// 			Member: item,
+// 		}}...)
+// 	}
 
-	err = autoupdater.RunRecentVisitQueue(ctx)
-	assert.Nil(t, err)
-	count, err = rdb.ZCard(ctx, autoupdater.RecentVisitQueue.Key).Result()
-	assert.EqualValues(t, 50-itemsGot-len(items)/2, count)
-	assert.Nil(t, err)
-}
+// 	err = autoupdater.RunRecentVisitQueue(ctx)
+// 	assert.Nil(t, err)
+// 	count, err = rdb.ZCard(ctx, autoupdater.RecentVisitQueue.Key).Result()
+// 	assert.EqualValues(t, 50-itemsGot-len(items)/2, count)
+// 	assert.Nil(t, err)
+// }
