72f7c1052927195d77002ae096a2fae6317bb9d2
KallyDev <36319157+kallydev@users.noreply.github.com>
2022-05-31 17:16:33 +0800

    feat(shared): log index field

--- shared/database/model/cache.go
+++ shared/database/model/cache.go
@@ -11,6 +11,7 @@ type Cache struct {
 	Network  string          `gorm:"column:network;primaryKey"`
 	Source   string          `gorm:"column:source;primaryKey"`
 	BlockNum int64           `gorm:"column:block_num;index:index_block_num"`
+	LogIndex int             `gorm:"column:log_index;primaryKey"`
 	Data     json.RawMessage `gorm:"column:data;type:jsonb"`
 
 	common.Table
